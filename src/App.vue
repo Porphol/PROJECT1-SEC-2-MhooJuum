e669c9565a0e71284e091eb64ce48759d1847cc6
Phurin Suwannarat <pierce.phurin@gmail.com>
2025-01-24 02:25:42 +0700

    123 - [update] test function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const totalTime = 5;
+const remainTime = ref(totalTime);
+
+const countTime = () => {
+  const interval = setInterval(() => {
+    if (remainTime.value > 0) {
+      remainTime.value--;
+      console.log(remainTime.value);
+    } else {
+      console.log('time out');
+      clearInterval(interval);
+    }
+  }, 1000);
+};
+
+countTime();
 </script>
 
 <template>
   <div>
-    <h1 class="text-7xl text-yellow-500">MhooJuum</h1>
+    {{ Math.floor(remainTime / 60) }}:{{ remainTime % 60 < 10 ? '0' : ''}}{{ remainTime % 60 }}
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
