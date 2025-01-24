79611eaa9025c1f111766f19730a444e3f615354
Porphol <142778106+Porphol@users.noreply.github.com>
2025-01-24 15:22:50 +0700

    068-[add] function score

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
+  import { ref } from "vue";
 
+  const score = ref(0)
+  const hitObject = () => {
+    score.value++
+    console.log(score.value);
+    
+  }
 </script>
 
 <template>
-  <div>
-    <h1 class="text-7xl text-yellow-500">MhooJuum</h1>
+  <div class="my-4">
+    <p>Score: <span>{{ score }}</span></p>
+  </div>
+  <div  v-on:click="hitObject" class="border border-red-600 bg-red-400 px-3 py-1 text-white text-xl inline-block">
+    <p>Mhoo</p>
   </div>
 </template>
 
