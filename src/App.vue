225db81976ffb463b804100da7f933836a65e1c6
Varinporn Kiratipattanakorn <varinporn.kira@kmutt.ac.th>
2025-01-24 13:25:18 +0700

    076 - [add] click-to-play

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script setup>
+import { ref } from "vue";
 
+const isHomeVisible = ref(true);
+
+const handlePlayButtonClick = () => {
+  isHomeVisible.value = !isHomeVisible.value;
+};
 </script>
 
 <template>
-  <div>
-    <h1 class="text-7xl text-yellow-500">MhooJuum</h1>
+  <div class="w-full max-w-4xl">
+    <div v-show="isHomeVisible" class="border">
+      <p>mhoojuum</p>
+      <button
+        @click="handlePlayButtonClick"
+        class="py-1 px-3 bg-yellow-200 rounded-lg"
+      >
+        PLAY
+      </button>
+    </div>
+
+    <div v-show="!isHomeVisible" class="border">
+      <p>game</p>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
