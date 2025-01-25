75b31b0911b5b5f260e3d0769dc00a5abfbf6679
pheamawas <pheamawas@gmail.com>
2025-01-25 20:36:36 +0700

    104-[add] function random position

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
+import { ref } from 'vue'
+
+const position = ref(0)
+
+const random = (num) =>{
+  position.value = Math.floor(Math.random() * num) + 1
+}
 
 </script>
 
 <template>
-  <div>
-    <h1 class="text-7xl text-yellow-500">MhooJuum</h1>
-  </div>
+    <div v-for="hole in 9" :key="hole">
+      <h1 class="text-7xl text-yellow-500" v-show="position === hole">JuumMhoo</h1>
+      <h1 class="text-7xl text-yellow-500" v-show="position !== hole">MhooJuum</h1>
+    </div>
+    <button v-on:click="random(9)">hey</button>
 </template>
 
 <style scoped>
