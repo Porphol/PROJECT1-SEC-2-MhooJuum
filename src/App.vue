13a03f2497984e08a57dc815748fa208cffbbd9d
Ppiercez <pierce.phurin@gmail.com>
2025-01-25 21:04:18 +0700

    123 - [update] function countDown

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref } from "vue";
 
-const totalTime = 5;
-const remainTime = ref(totalTime);
+const time = ref(0);
 
-const countTime = () => {
+const test = () => {
+  console.log(test);
+};
+const countDown = (func, duration, delay = 1000) => {
+  time.value = duration;
   const interval = setInterval(() => {
-    if (remainTime.value > 0) {
-      remainTime.value--;
-      console.log(remainTime.value);
-    } else {
-      console.log('time out');
+    func();
+    time.value--;
+    if (time.value === 0) {
+      console.log("time out");
       clearInterval(interval);
     }
-  }, 1000);
+  }, delay);
 };
 
-countTime();
+countDown(test, 5);
 </script>
 
 <template>
-  <div>
-    {{ Math.floor(remainTime / 60) }}:{{ remainTime % 60 < 10 ? '0' : ''}}{{ remainTime % 60 }}
+  <div class="text-7xl">
+    {{ Math.floor(time / 60) }}:{{ time % 60 < 10 ? "0" : "" }}{{ time % 60 }}
   </div>
 </template>
 
