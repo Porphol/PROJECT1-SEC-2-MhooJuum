f13e7d99a4c7fa0ac68a85d21a71c114d4c70390
Varinporn Kiratipattanakorn <varinporn.kira@kmutt.ac.th>
2025-01-27 21:35:24 +0700

    076 - [add] toggleModal function

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,46 @@
 <script setup>
-import { ref } from "vue"
+import { ref } from "vue";
 
-const currentView = ref("home")
+const currentView = ref("home");
 const handleButtonClick = (view) => {
-  currentView.value = view
-}
+  currentView.value = view;
+};
 
-const score = ref(0)
+const score = ref(0);
 const hitObject = () => {
-  score.value++
-}
+  score.value++;
+};
 
-const position = ref(0)
+const position = ref(0);
 const random = (num) => {
-  position.value = Math.floor(Math.random() * num) + 1
-}
+  position.value = Math.floor(Math.random() * num) + 1;
+};
 
-const time = ref(0)
-
+const time = ref(0);
 const countDown = (func, duration, delay = 1000) => {
-  time.value = duration
+  time.value = duration;
   const interval = setInterval(() => {
-    func()
-    time.value--
+    func();
+    time.value--;
     if (time.value <= 0) {
-      clearInterval(interval)
+      clearInterval(interval);
+      toggleModal("Time's up", `Your final score is ${score.value}`);
     }
-  }, delay)
-}
+  }, delay);
+};
+
+const showModal = ref(false);
+const modalTitle = ref("");
+const modalMessage = ref("");
+const toggleModal = (title, message) => {
+  modalTitle.value = title;
+  modalMessage.value = message;
+  showModal.value = !showModal.value;
+};
 </script>
 
 <template>
+  <!-- Home -->
   <div class="w-full max-w-4xl">
     <div v-show="currentView === 'home'" class="border">
       <p>mhoojuum</p>
@@ -42,13 +52,40 @@ const countDown = (func, duration, delay = 1000) => {
       </button>
       <br />
       <button
-        @click="handleButtonClick('howToPlay')"
+        @click="
+          toggleModal(
+            'How to Play',
+            'Here are the instructions for how to play the game:'
+          )
+        "
         class="py-1 px-3 bg-yellow-200 rounded-lg"
       >
         HOW TO PLAY
       </button>
     </div>
 
+    <!-- Modal -->
+    <div
+      v-if="showModal"
+      class="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center"
+      @click="toggleModal('', '')"
+    >
+      <div
+        class="bg-white p-6 rounded-lg shadow-lg max-w-xl w-full"
+        @click.stop
+      >
+        <h2 class="text-3xl mb-4">{{ modalTitle }}</h2>
+        <p class="mb-4">{{ modalMessage }}</p>
+        <button
+          @click="toggleModal('', '')"
+          class="py-2 px-4 bg-yellow-500 rounded-lg text-white mt-4"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+
+    <!-- Game -->
     <div v-show="currentView === 'game'" class="border">
       <p>game</p>
       <div>
@@ -74,16 +111,6 @@ const countDown = (func, duration, delay = 1000) => {
         class="py-1 px-3 bg-yellow-200 rounded-lg"
       >
         Back
-      </button>
-    </div>
-
-    <div v-show="currentView === 'howToPlay'" class="border">
-      <p>How to play</p>
-      <button
-        @click="handleButtonClick('home')"
-        class="py-1 px-3 bg-yellow-200 rounded-lg"
-      >
-        BACK
       </button>
     </div>
   </div>
